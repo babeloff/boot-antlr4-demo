43b8cb65b4ab4f5026d354d1539db2e8903b1c0f
Fredrick Eisele <phreed@gmail.com>
2017-10-30 17:15:07 -0500

    moved logic back to boot-antlr4

--- src/clj/babeloff/boot_antlr4_parser.clj
+++ src/clj/babeloff/boot_antlr4_parser.clj
@@ -32,64 +32,6 @@
   (->> (fs/split file-path)
        (map #(case % ".." "dots" "." "dot" %))))
 
-(defn parse-file
-  "A function that runs the ANTLR4 parser."
-  [fileset next-handler input start-rule target-dir & options]
-  (let [{:keys [encoding
-                show
-                tokens
-                lisp
-                edn
-                rdf
-                postscript
-                trace
-                diagnostics
-                sll]} options
-        target-dir-str (target-dir-str (.getCanonicalPath target-dir))]
-    (boot/empty-dir! target-dir)
-    (util/info "target: %s\n" target-dir-str)
-    (util/info "working directory: %s\n"
-      (-> (Paths/get "." (make-array String 0))
-         .toAbsolutePath .normalize .toString))
-
-    (let [lexer-inst (ANTLRv4Lexer. nil)
-          parser-inst (ANTLRv4Parser. nil)
-          char-set (Charset/defaultCharset)]
-     (doseq [file-path input]
-       (util/info "input: %s\n" file-path)
-       (let [tgt-file-path (apply io/file target-dir (get-target-path file-path))
-             src-file (Paths/get file-path (make-array String 0))
-             char-stream (CharStreams/fromPath src-file char-set)
-             _ (.setInputStream lexer-inst char-stream)
-             token-stream (CommonTokenStream. lexer-inst)]
-
-          (antlr/initialize-parser
-            file-path token-stream tokens tgt-file-path
-            lexer-inst parser-inst
-            diagnostics sll
-            tree edn rdf postscript)
-
-          (try
-            (antlr/handle-parse-tree
-              (Reflector/invokeInstanceMember parser-inst start-rule)
-              tgt-file-path parser-inst
-              tree edn rdf postscript)
-
-            (catch NoSuchMethodException ex
-                  (util/info "no method for rule %s or it has arguements \n"
-                              start-rule))
-            (finally
-                    (util/info "releasing %s\n" file-path))))))
-                    ;; prepare fileset and call next-handler
-    (let [fileset' (-> fileset
-                        (boot/add-asset target-dir)
-                        boot/commit!)
-          result (next-handler fileset')]
-      ;; post processing
-      ;; the goal here is to perform any side effects
-      result)))
-
-
 (deftask parse
   "A task that runs the ANTLR4 parser."
   [r start-rule     LIB_DIR str    "the name of the first rule to match"
@@ -103,19 +45,51 @@
    z postscript             bool   "produce a postscript output of the parse tree"
    x trace                  bool   "show the progress that the parser makes"
    d diagnostics            bool   "show some diagnostics"
-   f sll                    bool   "use the fast SLL prediction mode"]
+   f sll                    bool   "use the fast SLL prediction mode"])
+(cond
+  (not parser)
+  (do
+    (boot.util/fail "The --parser argument is required")
+    (*usage*))
+
+  (not start-rule)
+  (do
+    (boot.util/fail "The --start-rule argument is required")
+    (*usage*))
+
+  :else
   (let [target-dir (boot/tmp-dir!)
-        target-dir-str (.getCanonicalPath target-dir)]
-    (fn middleware [next-handler]
-      (fn handler [fileset]
-        (parse-file fileset next-handler input start-rule target-dir
-          {:encoding encoding
-           :show show
-           :tokens tokens
-           :lisp tree
-           :edn edn
-           :rdf rdf
-           :postscript postscript
-           :trace trace
-           :diagnostics diagnostics
-           :sll sll})))))
+          target-dir-str (.getCanonicalPath target-dir)]
+      (fn middleware [next-handler]
+        (fn handler [fileset]
+
+          (boot/empty-dir! target-dir)
+          (util/info "target: %s\n" target-dir-str)
+          (util/info "parser: %s\n" parser)
+          (util/info "lexer: %s\n" lexer)
+          (util/info "working directory: %s\n"
+            (-> (Paths/get "." (make-array String 0))
+               .toAbsolutePath .normalize .toString))
+
+         (when show
+           (util/info "parse options: %s\n" *opts*))
+
+         (let [lexer-inst (ANTLRv4Lexer. nil)
+               parser-inst (ANTLRv4Parser. nil)
+               char-set (Charset/defaultCharset)
+               target-file-fn identity]
+
+           (anltr/parse-file lexer-inst parser-inst char-set
+              start-rule input target-file-fn
+              {:encoding encoding :show show :tokens tokens
+                :lisp tree :edn edn :rdf rdf :postscript postscript
+                :trace trace :diagnostics diagnostics :sll sll}))
+
+            ;; prepare fileset and call next-handler
+         (let [fileset' (-> fileset
+                             (boot/add-asset target-dir)
+                             boot/commit!)
+               result (next-handler fileset')]
+           ;; post processing
+           ;; the goal here is to perform any side effects
+           result)))))
